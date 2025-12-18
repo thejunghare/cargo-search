b6beaf737489cdb1219cbc0b74f08ec94379191e
thejunghare <prasadjunghare2@gmail.com>
2025-12-18 12:14:11 +0530

    updated keyboard shortcuts

--- src/view/keyboard.js
+++ src/view/keyboard.js
@@ -1,97 +1,40 @@
-const mousetrap = require('mousetrap');
+// src/view/keyboard.js (or wherever this file is)
+
+// 1. Use 'remote' to access backend features from the frontend
+const { Menu, MenuItem, dialog } = require('@electron/remote');
 
 module.exports = (emitter, state) => {
-  mousetrap.bind(['alt'], () => {
-    emitter.emit('tabs-toggle');
-    return false;
-  });
+  // 2. Create the Menu
+  const menu = new Menu();
 
-  mousetrap.bind(['command+shift+t', 'ctrl+shift+t'], () => {
-    emitter.emit('dark-mode');
-    return false;
-  });
-
-  mousetrap.bind(['command+m', 'ctrl+m'], () => {
-    emitter.emit('menu-toggle');
-    return false;
-  });
-
-  mousetrap.bind(['command+h', 'ctrl+h'], () => {
-    emitter.emit('history-toggle');
-    return false;
-  });
-
-  mousetrap.bind(['command+shift+d', 'ctrl+shift+d'], () => {
-    emitter.emit('webview-devtools');
-    return false;
-  });
-
-  mousetrap.bind(['command+shift+a', 'ctrl+shift+a'], () => {
-    emitter.emit('webview-about');
-    return false;
-  });
-
-  Mousetrap.bind(['command+left', 'ctrl+left'], () => {
-    emitter.emit('webview-back');
-    return false;
-  });
-
-  Mousetrap.bind(['command+right', 'ctrl+right'], () => {
-    emitter.emit('webview-forward');
-    return false;
-  });
-
-  Mousetrap.bind(['command+r', 'ctrl+r', 'f5'], () => {
-    emitter.emit('webview-reload');
-    return false;
-  });
-
-  Mousetrap.bind(['command+shift+h', 'ctrl+shift+h'], () => {
-    emitter.emit('webview-home');
-    return false;
-  });
-
-  Mousetrap.bind(['command+t', 'ctrl+t'], () => {
-    emitter.emit('tabs-create');
-    return false;
-  });
-
-  Mousetrap.bind(['command+w', 'ctrl+w'], () => {
-    emitter.emit('tabs-remove-current');
-    return false;
-  });
-
-  Mousetrap.bind(['command+shift+left', 'ctrl+shift+left'], () => {
-    emitter.emit('tabs-prev');
-    return false;
-  });
-
-  Mousetrap.bind(['command+shift+right', 'ctrl+shift+right'], () => {
-    emitter.emit('tabs-next');
-    return false;
-  });
-
-  Mousetrap.bind(['command+0', 'ctrl+0'], () => {
-    emitter.emit('tabs-last');
-    return false;
-  });
-
-  let fullscreen = false;
-
-  Mousetrap.bind(['f11'], () => {
-    const remote = require('electron').remote;
-    let w = remote.getCurrentWindow();
-
-    fullscreen = !fullscreen;
-    w.setFullScreen(fullscreen);
-
-    return false;
-  });
-
-  for (let i = 1; i <= 9; i++) {
-    Mousetrap.bind(['command+' + i, 'ctrl+' + i], () => {
-      emitter.emit('tabs-go-to', i - 1);
-      return false;
-    });
+  // 3. macOS App Menu Fix (Required for Mac)
+  if (process.platform === 'darwin') {
+    const appMenu = new MenuItem({ role: 'appMenu' });
+    menu.append(appMenu);
   }
+
+  // 4. Build your Custom Submenu
+  const submenu = Menu.buildFromTemplate([
+    {
+      label: 'Open a Dialog',
+      click: () => dialog.showMessageBox({ message: 'Hello World!' }),
+      accelerator: 'CommandOrControl+Alt+R'
+    },
+    // Example: Re-adding your old 'New Tab' functionality using native menus
+    {
+      label: 'New Tab',
+      click: () => emitter.emit('tabs-create'),
+      accelerator: 'CommandOrControl+T'
+    },
+    {
+      label: 'Close Tab',
+      click: () => emitter.emit('tabs-remove-current'),
+      accelerator: 'CommandOrControl+W'
+    }
+  ]);
+
+  menu.append(new MenuItem({ label: 'Custom Menu', submenu }));
+
+  // 5. Apply the Menu
+  Menu.setApplicationMenu(menu);
 };
